1be7637c3f602b31f7455b73f7bb5c2cd9be8801
Dilan Morar <DMorar@spartaglobal.com>
2019-12-03 15:34:33 +0000

    exercise102 magic number attempted

---
+++ python_exercises/magic number 102.py
@@ -0,0 +1,22 @@
+# Magic number game!
+
+# I want you to use operators
+# equate something
+
+# As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
+# We should define/assign number to a variable called magic_number
+
+# magic_number =
+magic_number = 15
+
+# I need to ask user for input
+number = int(input('Choose a number between 0-10: '))
+
+# I need to check if this input matches a magic_number
+if magic_number == number:
+    # I need to let the user know if the response was write or not
+    print('You guessed the number correctly!')
+else:
+    print('You were wrong')
+
+#self five
